42601f01742178daa7e1ad5989a9b4c8631d550f
rafa16w34 <rafael16alvesf@gmail.com>
2025-03-31 23:52:39 -0300

    Versao inicial

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,116 @@
+alert("Ce ta baum meu fi?"); // alert exibe um aviso na guia
+let pontos = 0;
+let maximo = 100;
+let minimo = 1;
+let numeroSecreto = parseInt(Math.random()*maximo+minimo); //let serve para criar uma variavel
+let chute;              //prompt aparece, além de um aviso na guia, um espaço para escrever
+console.log(numeroSecreto);//mensagem secreta na aba inspecionar
+let tentativas = 1;
+
+while(chute != numeroSecreto){//enquanto
+
+    chute = prompt ("Escolha um número entre " + minimo + " e " + maximo);
+
+    if (isNaN(chute) || chute.length == 0){
+        alert("Cê é besta hein!");
+    }else{
+
+    if (numeroSecreto == chute){//se o chute for igual ao número secreto...
+
+        alert("Gotcha, você acertou o número era " + numeroSecreto +"!");
+        pontos = 100;
+
+        }else{
+        if (chute > numeroSecreto){
+            alert ("O número é menor que " + chute + "." );  
+        }
+        else{
+            alert ("O número é maior que " + chute + "." );   
+        }
+        pontos = 0;
+        tentativas++;
+        }       
+}}
+
+let palavraVez = tentativas > 1 ? " vezes." : " vez.";//operador ternário
+alert("Você tentou " + tentativas + palavraVez)
+
+
+
+//Exercício da alura que não era necessário!
+//let nome = Lua;
+//let idade = 25;
+//let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro =  ("Erro! Preencha todos os campos");
+alert (mensagemDeErro);
+let nome = prompt ("Nome:");
+let idade = prompt ("Idade:");
+if (idade >= 18){
+    alert("Pode tirar a habilitação!");
+}else {
+    alert("Você é fraco, lhe falta ódio!");
+}
+
+//Exercício da alura que não era necessário!
+diaDaSemana = prompt("Qual o dia da semana?");
+if (diaDaSemana == "Sabado" || diaDaSemana == "Sábado" || diaDaSemana == "sábado" || diaDaSemana == "sabado" || diaDaSemana == "Domingo" || diaDaSemana == "domingo"){
+alert("Bom fim de semana!");
+}else{
+    alert("Boa semana!");
+}
+if (chute >= 0){
+   alert("O número digitado é um número positivo!");
+}else{
+    alert("O número digitado é um número negativo!");
+}
+if (pontos == 100){
+    alert ("Parabéns, você ganhou com a pontuação de " + pontos);
+}else{
+   alert ("Você perdeu com a pontuação de" + pontos);
+}
+alert("O saldo da conta é " + saldoDisponivel + ".");
+alert("Seja bem vindo " + nome + "!");
+
+//Exercício da alura que não era necessário!
+let numero = 0;
+while (numero != 11){
+    console.log(numero);
+   alert(numero);
+    numero++;
+}
+let numeroo = 10;
+while (numeroo != -1){
+   console.log(numeroo);
+    alert(numeroo);
+    numeroo--;
+}
+
+//Exercício da alura que não era necessário!
+console.log("Bem vindo!");
+console.log("Olá " + nome + " !");
+let lingDeProg = prompt("Qual linguagem de programação você mais gosta?");
+console.log(lingDeProg);
+valor1 = 2;
+valor2 = 3;
+resultado = valor1 + valor2;
+console.log("A soma de " + valor1 + " e " + valor2 + " é igual a " + resultado);
+resultado2 = valor1 - valor2;
+console.log("A diferença de " + valor1 + " e " + valor2 + " é igual a " + resultado2);
+if (idade >= 18){
+    alert("Você é de maior!");
+}else {
+    alert("Você é fraco, lhe falta ódio!");
+}
+let nota = parseInt(Math.random()*10+1);
+if (nota >= 7){
+    alert("Aprovado pois sua nota é igual a "+ nota +".");
+}else{
+    alert("Reprovado pois sua nota é igual a "+ nota +".");
+}
+let numeroConsole = Math.random();
+console.log(numeroConsole);
+let numeroConsole2 = parseInt(Math.random()*10+1);
+console.log(numeroConsole2);
+let numeroConsole3 = parseInt(Math.random()*1000+1);
+console.log(numeroConsole3);
